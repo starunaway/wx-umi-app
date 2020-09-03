4a9b6f8a5f60380a02a6ace077394ebe63d9f5ac
keeping <starunaway@gmail.com>
2020-09-03 10:06:28 +0800

    feat: 🎸 init

---
+++ wx-uni/pages/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<view :class="test" @click="click">
+		hhhh
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				test:'test'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+click(){
+	console.log('222222222');
+}
+		}
+	}
+</script>
+
+<style>
+	.test{
+		color: red;
+	}
+	
+</style>
