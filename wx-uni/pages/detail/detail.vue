4f854a45797cf0465dd0aa1eaec14414361d9951
keeping <starunaway@gmail.com>
2020-09-05 16:03:12 +0800

    feat: 🎸 add pages

---
+++ wx-uni/pages/detail/detail.vue
@@ -0,0 +1,18 @@
+<template>
+	<view class="">
+		<view >
+		<text>			dance rap		</text>
+		</view>
+		<view class="">			
+		<text selectable="true">
+			dance rap
+		</text>
+		</view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
