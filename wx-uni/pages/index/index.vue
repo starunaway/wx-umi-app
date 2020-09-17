81b896c2302e9c4e658a1a0825362413963a5f7d
keeping <starunaway@gmail.com>
2020-09-17 23:12:57 +0800

    feat: 🎸 get

--- wx-uni/pages/index/index.vue
+++ wx-uni/pages/index/index.vue
@@ -1,13 +1,30 @@
 <template>
-	<view class="">
-		首页
-	</view>
+  <view class=""> 首页 </view>
 </template>
+
 <script>
-	export default {
-		data(){
-			return {}
-		}
+import fetch from '../../api/fetch.js';
+export default {
+  data() {
+    return {
+      swipers: [],
+    };
+  },
+  methods: {
+    async getSwipers() {
+      let result = await fetch.get('https://dog.ceo/api/breeds/image/random');
+      if (((result || {}).data || {}).status !== 0) {
+        uni.showToast({
+          title: '获取数据失败',
+        });
+      } else {
+        //   this.swipers = result.data.swipers
+        this.swipers = [];
+      }
+    },
+  },
+	onLoad() {
+		this.getSwipers()
 	}
-	
+};
 </script>
