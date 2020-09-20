a585f8bd2ac01e065d6157cd6118c97b6334bc21
keeping <starunaway@gmail.com>
2020-09-20 17:40:24 +0800

    feat: 🎸 news-item components

---
+++ wx-uni/components/news-item/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <view class="news-item">
+    <image :src="item.img_url" mode=""></image>
+    <view class="right">
+      <view class="tit">{{ item.title }}</view>
+      <view class="info">
+        <text>发表时间：{{ item.add_time }}</text>
+        <text>浏览:{{ item.click }}</text>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  props: ['item'],
+};
+</script>
+
+<style lang="scss">
+.news-item {
+  display: flex;
+  padding: 10rpx 20rpx;
+  border-bottom: 1rpx solid $shop-color;
+  image {
+    min-width: 200rpx;
+    max-width: 200rpx;
+    height: 150rpx;
+  }
+  .right {
+    margin-left: 15rpx;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    .tit {
+      font-size: 30rpx;
+    }
+    .info {
+      font-size: 24rpx;
+      text:nth-child(2) {
+        margin-left: 30rpx;
+      }
+    }
+  }
+}
+</style>
--- wx-uni/pages/news/news.vue
+++ wx-uni/pages/news/news.vue
@@ -1,21 +1,16 @@
 <template>
   <view class="news">
-    <view class="news-item" v-for="(item, index) in newsList" :key="index">
-      <image :src="item.img_url" mode=""></image>
-      <view class="right">
-        <view class="tit">{{ item.zhaiyao }}</view>
-        <view class="info">
-          <text>发表时间：{{ item.add_time }}</text>
-          <text>浏览:{{ item.click }}</text>
-        </view>
-      </view>
-    </view>
+    <newsList v-for="item in newsList" :key="item.id" :item="item"></newsList>
   </view>
 </template>
 
 <script>
 import {mockGoods} from '../../mockData/data.js';
+import newsList from '../../components/news-item/index.vue';
 export default {
+  components: {
+    newsList,
+  },
   data() {
     return {
       newsList: [],
@@ -34,31 +29,5 @@ export default {
 </script>
 
 <style lang="scss">
-.news {
-  .news-item {
-    display: flex;
-    padding: 10rpx 20rpx;
-    border-bottom: 1rpx solid $shop-color;
-    image {
-      min-width: 200rpx;
-      max-width: 200rpx;
-      height: 150rpx;
-    }
-    .right {
-      margin-left: 15rpx;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      .tit {
-        font-size: 30rpx;
-      }
-      .info {
-        font-size: 24rpx;
-				text:nth-child(2){
-					margin-left: 30rpx;
-					}
-      }
-    }
-  }
-}
+
 </style>
