6240e383cf42fec777e91b4815b4142228f54eea
keeping <starunaway@gmail.com>
2020-09-20 17:00:09 +0800

    feat: 🎸 pics

---
+++ wx-uni/mockData/picsCategory.js
@@ -0,0 +1,38 @@
+export const picsCategory = [
+	{
+		title:"家居生活",
+		id:1
+	},
+	{
+		title:"摄影设计",
+		id:2
+	},
+	{
+		title:"明星美女",
+		id:3
+	},	{
+		title:"空间设计",
+		id:4
+	},	{
+		title:"户外装饰",
+		id:5
+	},	{
+		title:"广告摄影",
+		id:6
+	},	{
+		title:"摄影学习",
+		id:7
+	},	{
+		title:"摄影器材",
+		id:8
+	},	{
+		title:"明星写真",
+		id:9
+	},	{
+		title:"我爱我家",
+		id:10
+	},	{
+		title:"可爱宠物",
+		id:11
+	},
+]
--- wx-uni/pages/pics/pics.vue
+++ wx-uni/pages/pics/pics.vue
@@ -1,8 +1,95 @@
 <template>
+  <view class="pics">
+    <scroll-view scroll-y="true" class="left">
+      <view
+        :class="index === active ? 'active' : ''"
+        @click="leftClick(index, item)"
+        v-for="(item, index) in cates"
+        :key="item.id"
+        >{{ item.title }}</view
+      >
+    </scroll-view>
+
+    <scroll-view scroll-y="true" class="right">
+      <view class="item" v-for="item in secondData" :key="item.id">
+        <image :src="item.img_url" mode=""></image>
+        <text>{{ item.title }}</text>
+      </view>
+    </scroll-view>
+  </view>
 </template>
 
 <script>
+import {picsCategory} from '../../mockData/picsCategory.js';
+import {mockGoods} from '../../mockData/data.js';
+export default {
+  data() {
+    return {
+      cates: [],
+      active: 0,
+      secondData: [],
+    };
+  },
+  methods: {
+    async getPicsCategory() {
+      let result = this.$fetch.get('https://dog.ceo/api/breeds/image/random');
+      this.cates = picsCategory;
+      this.secondData = mockGoods;
+    },
+    leftClick(index, item) {
+      // let result = this.$fetch.get(item.id);
+      this.active = index;
+      this.secondData = mockGoods;
+    },
+  },
+  onLoad() {
+    this.getPicsCategory();
+  },
+};
 </script>
 
-<style>
+<style lang="scss">
+page {
+  height: 100%;
+}
+.pics {
+  display: flex;
+  height: 100%;
+  .left {
+    width: 200rpx;
+    height: 100%;
+    view {
+      height: 60px;
+      line-height: 60px;
+      font-size: 30rpx;
+      text-align: center;
+      color: #333;
+
+      border-top: 1px solid #eee;
+      border-right: 1px solid #eee;
+    }
+    .active {
+      background-color: $shop-color;
+      color: #fff;
+    }
+  }
+
+  .right {
+    height: 100%;
+    width: 520rpx;
+    margin: 10rpx auto;
+    .item {
+      image {
+        display: block;
+        width: 520rpx;
+        height: 520rpx;
+        border-radius: 5px;
+      }
+      text {
+        font-size: 30rpx;
+        line-height: 60rpx;
+      }
+    }
+  }
+}
 </style>
